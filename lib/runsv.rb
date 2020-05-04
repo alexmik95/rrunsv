b6c8b88f8039eb799c9997c21c770d2917a6f58a
alexmik <alexmik95@gmail.com>
2020-05-04 16:16:05 +0200

    Runsv::Config class

--- lib/runsv.rb
+++ lib/runsv.rb
@@ -3,4 +3,5 @@ require "runsv/version"
 module Runsv
   class Error < StandardError; end
   # Your code goes here...
+  autoload :Config, "runsv/config"
 end
---
+++ lib/runsv/config.rb
@@ -0,0 +1,45 @@
+module Runsv
+  class Config
+    attr_reader :fullpath, :path, :filename, :lines
+
+    def initialize(args = {})
+      @fullpath = args.delete(:fullpath).to_s
+      @path = @fullpath.split("/")[0..-2].join("/")
+      @filename = @fullpath.split("/").last
+      @lines = []
+      ensure_directory
+      write(args.delete(:text).to_s)
+    end
+
+    def ensure_directory
+      system("mkdir -p #{@path}")
+    end
+
+    def read
+      File.read(@fullpath) if File.exist?(@fullpath)
+    end
+
+    def clear
+      if File.exist?(@fullpath)
+        File.delete(@fullpath)
+        @lines.clear
+      end
+    end
+
+    def write(text)
+      clear
+      lines = text.split("\n").compact.map(&:strip).reject(&:empty?)
+      add_lines(lines)
+    end
+
+    def add_lines(lines = [])
+      File.open(@fullpath, "a") do |file|
+        lines.each do |line|
+          file.write(line)
+          @lines << line
+        end
+      end
+      self
+    end
+  end
+end
